94b51dd13d2b352bc5b27787e1ea00735b406ee6
mhadam <michael@hadam.us>
2021-07-20 21:57:56 -0400

    Remove conflicting app_dir setting

--- app/princetonpy/settings.py
+++ app/princetonpy/settings.py
@@ -65,7 +65,6 @@ TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [os.path.join(BASE_DIR, "princetonpy", "templates")],
-        "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
                 "django.template.context_processors.debug",
