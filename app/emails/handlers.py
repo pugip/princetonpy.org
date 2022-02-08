f548871839a5c4eadf785a4251a198a40d4d2c0c
mhadam <michael@hadam.us>
2022-02-08 16:09:14 -0500

    Save email failures

--- app/emails/handlers.py
+++ app/emails/handlers.py
@@ -8,17 +8,19 @@ from emails.models import EmailFailure
 def bounce_handler(sender, mail_obj, bounce_obj, raw_message, *args, **kwargs):
     # message_id = mail_obj["messageId"]
     # recipient_list = mail_obj["destination"]
-    EmailFailure(
+    failure = EmailFailure(
         raw_message=raw_message,
         failure_type=EmailFailure.BOUNCE
     )
+    failure.save()
 
 
 @receiver(complaint_received)
 def complaint_handler(sender, mail_obj, complaint_obj, raw_message, *args, **kwargs):
     # message_id = mail_obj["messageId"]
     # recipient_list = mail_obj["destination"]
-    EmailFailure(
+    failure = EmailFailure(
         raw_message=raw_message,
         failure_type=EmailFailure.COMPLAINT
     )
+    failure.save()
