d8131744bb04282e086700ffd8ac39ac862bd518
mhadam <michael@hadam.us>
2022-02-08 13:13:36 -0500

    Add app for emails

---
+++ app/emails/handlers.py
@@ -0,0 +1,24 @@
+from django.dispatch import receiver
+from django_ses.signals import bounce_received, complaint_received
+
+from emails.models import EmailFailure
+
+
+@receiver(bounce_received)
+def bounce_handler(sender, mail_obj, bounce_obj, raw_message, *args, **kwargs):
+    # message_id = mail_obj["messageId"]
+    # recipient_list = mail_obj["destination"]
+    EmailFailure(
+        raw_message=raw_message,
+        failure_type=EmailFailure.BOUNCE
+    )
+
+
+@receiver(complaint_received)
+def complaint_handler(sender, mail_obj, complaint_obj, raw_message, *args, **kwargs):
+    # message_id = mail_obj["messageId"]
+    # recipient_list = mail_obj["destination"]
+    EmailFailure(
+        raw_message=raw_message,
+        failure_type=EmailFailure.COMPLAINT
+    )
