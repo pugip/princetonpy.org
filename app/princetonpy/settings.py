d8ae3afb177e202169d1066a09254da5f5076868
mhadam <michael@hadam.us>
2022-03-21 01:02:32 -0400

    Fix missing css files & settings

--- app/princetonpy/settings.py
+++ app/princetonpy/settings.py
@@ -14,8 +14,8 @@ import os
 from pathlib import Path
 
 import environ
+
 # Set the project base directory
-from whitenoise.media_types import default_types
 
 BASE_DIR = Path(__file__).resolve().parents[1]  # /princetonpy.org/app
 REPO_DIR = BASE_DIR.parent  # /princetonpy.org
@@ -198,5 +198,3 @@ CRONJOBS = [
     ("0 0 * * *", "princetonpy.cron.backup.save_users"),
     ("*/5 * * * *", "django.core.management.call_command", ["submit_newsletter"]),
 ]
-
-WHITENOISE_MIMETYPES = default_types()
