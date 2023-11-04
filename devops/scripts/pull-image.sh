63e1731044700d59a6633ac99d94afe0c6f0a74e
mhadam <michael@hadam.us>
2023-11-04 15:56:06 -0400

    Add exec bit to pull-image.sh

--- devops/scripts/pull-image.sh
+++ devops/scripts/pull-image.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-</home/princeton/ghtoken docker login ghcr.io --username mhadam --password-stdin
+</home/princeton/ghtoken /usr/bin/docker login ghcr.io --username mhadam --password-stdin
 /usr/bin/docker pull ghcr.io/pugip/princetonpy.org/web:release
 /usr/bin/docker pull ghcr.io/pugip/princetonpy.org/cron:release
