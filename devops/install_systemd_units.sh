10c92de67a0bc0b4fded861cc18ac2662c71342d
mhadam <michael@hadam.us>
2023-11-04 16:49:07 -0400

    More deployment changes

--- devops/install_systemd_units.sh
+++ devops/install_systemd_units.sh
@@ -5,10 +5,8 @@ cp ./systemd/*.{service,path} /etc/systemd/system/
 systemctl daemon-reload
 
 systemctl enable deploy-caddy.{path,service}
-systemctl enable deploy-princetonpy.service
 systemctl enable princetonpy-cron.service
 systemctl enable princetonpy-web.service
 systemctl start deploy-caddy.{path,service}
-systemctl start deploy-princetonpy.service
 systemctl start princetonpy-cron.service
 systemctl start princetonpy-web.service
