b3896e8150c4cca365b1fec5c7753987736e0900
mhadam <michael@hadam.us>
2023-11-04 23:04:21 -0400

    Consolidate services

--- devops/install_systemd_units.sh
+++ devops/install_systemd_units.sh
@@ -5,8 +5,6 @@ cp ./systemd/*.{service,path} /etc/systemd/system/
 systemctl daemon-reload
 
 systemctl enable deploy-caddy.{path,service}
-systemctl enable princetonpy-cron.service
-systemctl enable princetonpy-web.service
+systemctl enable princetonpy.service
 systemctl start deploy-caddy.{path,service}
-systemctl start princetonpy-cron.service
-systemctl start princetonpy-web.service
+systemctl start princetonpy.service
--- devops/uninstall_systemd_units.sh
+++ devops/uninstall_systemd_units.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 systemctl disable deploy-caddy.{path,service}
-systemctl disable princetonpy-cron.service
-systemctl disable princetonpy-web.service
+systemctl disable princetonpy.service
 systemctl daemon-reload
 # rm ./systemd/deploy-caddy.{path,service}
